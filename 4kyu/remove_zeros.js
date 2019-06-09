c107976ebda4bbc69299a15686feb733faf6b46e
Vitya <plasticbottle@users.noreply.github.com>
2019-06-09 01:02:51 +0100

    Create remove_zeros.js

---
+++ 4kyu/remove_zeros.js
@@ -0,0 +1,25 @@
+//One Line Task: Remove Zeros
+
+/*
+Task:
+
+You are given an integer array a that contains only digits 0-9. Remove all zeros from the start and end of a.
+
+It is guaranteed that array a has at least two non-zero elements.
+Code Limit: Less than 53 characters.
+Examples:
+
+For a = [0, 9, 0, 4],
+
+the output should be [9, 0, 4].
+
+For a = [0, 9, 5, 0, 0, 0, 0, 2, 0, 0],
+
+the output should be [9, 5, 0, 0, 0, 0, 2].
+
+For a = [1, 6, 0, 2],
+
+the output should be [1, 6, 0, 2].
+*/
+
+removeZeros=n=>eval('['+/[1-9].*[1-9]/.exec(n)+']')
