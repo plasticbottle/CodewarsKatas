1a7f1f402ff03acaf8573e84f15b7bcf006dbafe
Vitya <plasticbottle@users.noreply.github.com>
2019-06-15 11:00:37 +0100

    Create dont_get_stressed.js

---
+++ 6kyu/dont_get_stressed.js
@@ -0,0 +1,20 @@
+function stress (word) {
+
+var result = null;
+var penEndings = ["ic", "sion", "tion", "ious"];
+var antiPenEndings = ["ty", "al", "ise", "ate"];
+
+var numberOfSyllables = Number(word.replace(/[^\d]*/g, ""));
+var ending = word.replace(/[\d]*/g, "");
+  
+  if (numberOfSyllables < 3 || numberOfSyllables > 11) {
+    result = "invalid word"
+  } else if (penEndings.indexOf(ending) != -1) {
+    result = numberOfSyllables-1 + 'pen';
+  } else if (antiPenEndings.indexOf(ending) != -1) {
+    result = numberOfSyllables-2 + 'ante-pen';
+  } else {
+    result = "invalid word"
+  }
+  return result;
+}
