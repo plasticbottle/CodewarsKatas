2216c1be2e6bde51744abc262010b9797b638b1d
Vitya <plasticbottle@users.noreply.github.com>
2019-06-15 12:08:15 +0100

    Create snail.php

---
+++ 4kyu/snail.php
@@ -0,0 +1,35 @@
+function snail(array $array): array {
+$result = [];
+$blocker = -1;
+$arrayLength = count($array);
+
+if(isset($array[0][0])) {
+    $result = (new SplFixedArray($arrayLength * $arrayLength))->toArray();
+    $x = 0;
+    $y = 0;
+    $a = 1;
+    $b = 0;
+        for($i = 0; $i < count($result); ++$i) {
+            $result[$i] = $array[$y][$x];
+            $array[$y][$x] = $blocker;
+            $x += $a; 
+            $y += $b;
+          
+            if($a != 0 && ($x < 0 || $x >= $arrayLength || $array[$y][$x] == $blocker)) {
+               $x += -$a; 
+               $y += $a;
+               $b = $a; 
+               $a = 0;
+            }
+            else if($b != 0 && ($y < 0 || $y >= $arrayLength || $array[$y][$x] == $blocker)) {
+               $x += -$b; 
+               $y += -$b;
+               $a = -$b; 
+               $b = 0;
+            }
+        }
+}
+    return $result;
+}
+
+//for problem description: check snail.js
