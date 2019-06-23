87dc9140a3c3f4438fdee3e1758c9ba25c39d8d4
Vitya <plasticbottle@users.noreply.github.com>
2019-06-23 08:23:29 +0100

    Create generate_algorithm_series_mutate.js

---
+++ 7kyu/generate_algorithm_series_mutate.js
@@ -0,0 +1,4 @@
+//const mutate = (chromosome, p) => chromosome.split('').map(x => {return (Math.random() <= p) ? (!parseInt(x)*1) : parseInt(x)}).join('');
+//https://www.codewars.com/kata/567b39b27d0a4606a5000057
+//It is going to check every position and by a given probability p it will decide if a mutation will occur.
+//A mutation is the change from 0 to 1 or from 1 to 0.
