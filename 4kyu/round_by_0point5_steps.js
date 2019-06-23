310e8d0853ca17a76793a70d03371f1ea00a0634
Vitya <plasticbottle@users.noreply.github.com>
2019-06-23 09:57:44 +0100

    Create round_by_0point5_steps.js

---
+++ 4kyu/round_by_0point5_steps.js
@@ -0,0 +1,17 @@
+var solution = n => Math.round(n*2)/2;
+//https://www.codewars.com/kata/51f1342c76b586046800002a
+
+/*
+ound any given number to the closest 0.5 step
+
+I.E.
+
+solution(4.2) = 4
+solution(4.3) = 4.5
+solution(4.6) = 4.5
+solution(4.8) = 5
+
+Round up if number is as close to previous and next 0.5 steps.
+
+solution(4.75) == 5
+*/
